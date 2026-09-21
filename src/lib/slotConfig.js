fbbf84350f14b14a610bde2a18e4eb5176ce0c04
crydo5-apps <crydo5.apps@gmail.com>
2026-09-21 15:14:39 +0200

    Add turbo spin duration and delays constants

--- src/lib/slotConfig.js
+++ src/lib/slotConfig.js
@@ -32,6 +32,11 @@ export const REEL_COUNT = 3;
 export const ROW_COUNT = 3;
 export const SPIN_DURATION = 1600; // ms bis alle Walzen stehen
 export const REEL_DELAYS = [800, 1150, 1500]; // Stopp-Zeiten pro Walzel
+export const SPIN_DURATION = 1600;
+export const REEL_DELAYS = [800, 1150, 1500];
+export const SPIN_DURATION_TURBO = 650;
+export const REEL_DELAYS_TURBO = [300, 450, 600];
+
 
 // 🎰 DIE 5 PAYLINES
 export const PAYLINES = [
